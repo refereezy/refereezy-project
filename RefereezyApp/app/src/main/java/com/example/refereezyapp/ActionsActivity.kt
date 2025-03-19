2a37eb330ae580077df3c0d6d1eb3f0f4d3b5179
Santiago Muñoz <2023_santiago.munoz@iticbcn.cat>
2025-03-19 21:42:45 +0100

    Added some styling to the app

--- RefereezyApp/app/src/main/java/com/example/refereezyapp/ActionsActivity.kt
+++ RefereezyApp/app/src/main/java/com/example/refereezyapp/ActionsActivity.kt
@@ -1,23 +1,20 @@
 package com.example.refereezyapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ActionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_actions)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        startActivity(Intent(this, PairingClockActivity::class.java))
     }
 }
