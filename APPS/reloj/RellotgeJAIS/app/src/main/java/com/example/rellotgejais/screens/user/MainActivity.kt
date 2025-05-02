499699bd12535c5742af6e2aa2cc6827b1b1a2bb
Isma <2023_Ismael.younes@iticbcn.cat>
2025-05-02 19:26:34 +0200

    main activity reempezada

--- APPS/reloj/RellotgeJAIS/app/src/main/java/com/example/rellotgejais/screens/user/MainActivity.kt
+++ APPS/reloj/RellotgeJAIS/app/src/main/java/com/example/rellotgejais/screens/user/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.rellotgejais.MyApp
 import com.example.rellotgejais.R
 import com.example.rellotgejais.data.handlers.RefereeViewModel
@@ -24,7 +26,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         this.enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val nextActivity = findViewById<Button>(R.id.startBtn)
+        val nextActivity = findViewById<Button>(R.id.nextActivity)
+
+
+        // components
+        val loadingGif = findViewById<ImageView>(R.id.splash_logo)
+        Glide.with(this)
+            .load(R.raw.loading_football)
+            .into(loadingGif)
 
 
         /**
@@ -42,8 +51,6 @@ class MainActivity : AppCompatActivity() {
                 loadReport()
             }
         }
-
-
 
         nextActivity.setOnClickListener { v: View? ->
             refereeViewModel.getReferee(tempId, tempToken)
@@ -69,5 +76,10 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this@MainActivity, ActionsActivity::class.java)
         startActivity(intent)
 
+    }
+    fun goToLogin() {
+        val intent = Intent(this, WaitActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- APPS/reloj/RellotgeJAIS/app/src/main/java/com/example/rellotgejais/screens/user/WaitActivity.kt
+++ APPS/reloj/RellotgeJAIS/app/src/main/java/com/example/rellotgejais/screens/user/WaitActivity.kt
@@ -3,11 +3,9 @@ package com.example.rellotgejais.screens.user
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.rellotgejais.R
 
-class waitActivity : AppCompatActivity() {
+class WaitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
