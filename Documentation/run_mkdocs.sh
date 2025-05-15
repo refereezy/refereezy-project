d56b47cb627a80ff4a94e8729325347ddec9b4dc
Server <server@refereezy.smcardona.tech>
2025-05-15 22:19:50 +0000

    api

--- Documentation/run_mkdocs.sh
+++ Documentation/run_mkdocs.sh
@@ -1,3 +1,4 @@
 sudo docker build -t mi-mkdocs .
-sudo docker run -p 8000:8000 -v $(pwd):/docs mi-mkdocs
+#sudo docker run -p 8000:8000 -v $(pwd):/docs mi-mkdocs /bin/sh
+cd ..
 sudo docker-compose up -d
